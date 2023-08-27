06545fcdc1e2589a19098be818bb57a9cf708320
Giorgi Megrelishvili <gmegr18@freeuni.edu.ge>
2023-08-27 22:30:45 +0400

    Uploaded project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Structure
+
+![Structure](/structure.png)
---
+++ project_structure.py
@@ -0,0 +1,36 @@
+from diagrams import Cluster, Diagram, Edge
+from diagrams.elastic.elasticsearch import ElasticSearch
+from diagrams.ibm.user import Browser, User
+from diagrams.onprem.database import Mysql
+from diagrams.programming.framework import React, Spring
+from diagrams.programming.language import Go as GoLang
+
+with Diagram("Project Structure", filename="structure", strict=True):
+    with Cluster("Browser", direction="TB"):
+        user = User(label="User")
+        brower = Browser(label="Chrome's Extention")
+
+        user >> brower
+
+    with Cluster("Web Application"):
+        react = React(label="FrontEnd")
+        spring = Spring(label="BackEnd")
+
+        react >> spring
+        spring >> react
+
+        brower >> Edge(label="User Interface") >> react
+
+    with Cluster("Extention's Service"):
+        go = GoLang(label="Gin")
+        brower >> Edge(label="Rest API") >> go
+
+    with Cluster("Database", direction="TB"):
+        es = ElasticSearch(label="ElasticSearch")
+        mysql = Mysql(label="MySql")
+
+        es >> Edge(label="Protocol") >> mysql
+        spring >> es
+        spring >> Edge(label="Protocol") >> mysql
+        go >> es
+        go >> Edge(label="Protocol", style="dashed") >> mysql
